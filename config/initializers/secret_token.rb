484d224102d386fc5ddc7420f2c6fc8bda32e374
edwinhermawan <edwin@signaturepages.com>
2013-10-24 18:35:12 -0700

    Added rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stich::Application.config.secret_key_base = '947ac771482c347d534abffcd5ab000f763f6780c8019a5d2eca92bcf259a4a00db513cc931ea63b261b475cbc008584e4087c2a631bf7f3473389eb6bc9e0ec'
